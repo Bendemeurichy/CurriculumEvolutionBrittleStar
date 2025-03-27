8a90cc5801db84bc946ac57e7f427580dd8a4c25
Ben De Meurichy <48103261+Bendemeurichy@users.noreply.github.com>
2025-03-27 18:21:30 +0100

    ch ch ch changes"

---
+++ src/NEAT/model_loader.py
@@ -0,0 +1,7 @@
+import numpy as np
+import os
+from tensorneat.genome import DefaultGenome, BiasNode
+
+##########################################################################
+# module to load genomes for the neat library, modifiers to add nodes,...#
+##########################################################################
